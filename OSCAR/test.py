ec1611f00a78c3ddbc93631384d65e69f54dfc2c
David-Peede <jakepeede@gmail.com>
2023-01-18 00:07:40 -0500

    added readings

--- OSCAR/test.py
+++
@@ -1,5 +0,0 @@
-###comment out the line below
-print('Open this file .py file with Vi')
-
-###uncomment out the line below and re-run this python script
-#print('Yeah!! I did it!')
---
+++ spring-2k21/OSCAR/test.py
@@ -0,0 +1,5 @@
+###comment out the line below
+print('Open this file .py file with Vi')
+
+###uncomment out the line below and re-run this python script
+#print('Yeah!! I did it!')
